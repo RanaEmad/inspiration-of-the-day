637bab15bbf4a82156ae4008b598f41d7a19aef0
Rana Emad <rana3emad@gmail.com>
2020-07-20 16:18:14 +0200

    Updates

---
+++ precache-manifest.cca23e811b44d934c6ec71a10e87f5fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff95fc895ff145f86b632241101491e",
+    "url": "/inspiration-of-the-day/index.html"
+  },
+  {
+    "revision": "69b04159da4fd8058bc3",
+    "url": "/inspiration-of-the-day/static/css/main.7fba6a79.chunk.css"
+  },
+  {
+    "revision": "45bdf13ccfdea9564ae8",
+    "url": "/inspiration-of-the-day/static/js/2.d14f3907.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inspiration-of-the-day/static/js/2.d14f3907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b04159da4fd8058bc3",
+    "url": "/inspiration-of-the-day/static/js/main.1695041d.chunk.js"
+  },
+  {
+    "revision": "227ac3503cf70f840493",
+    "url": "/inspiration-of-the-day/static/js/runtime-main.d137b9aa.js"
+  }
+]);
